039ca1266a23af9e7e3c6a278bd88070c6e5ec7a
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-03-10 21:50:02 +0700

    update with upload file (images)

--- app/src/main/java/com/webview/MainActivity.java
+++ app/src/main/java/com/webview/MainActivity.java
@@ -1,10 +1,14 @@
 package com.webview;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,9 +17,13 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     //Define URL
-    private static final String URL = "http://updatesepati.blogspot.com/";
+    private static final String URL = "http://192.168.1.101/webview/";
     //Define WebView
     private WebView webView;
+
+
+    private ValueCallback<Uri> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +31,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         webView = (WebView)findViewById(R.id.webView); //get webView
         webView.setWebViewClient(new WebViewClient()); //set webView client
+
+        //set Upload File function
+        webView.setWebChromeClient(new WebChromeClient(){
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                //set file type
+                i.setType("image/*");
+                //launch chooser window
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
+
         WebSettings webSettings = webView.getSettings();// initiate webView settings
         webSettings.setJavaScriptEnabled(true); //allow webView perform javascript
         webSettings.setBuiltInZoomControls(true); //show zoom control
         webView.loadUrl(URL); //load URL
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
